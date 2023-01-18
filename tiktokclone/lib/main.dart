8b5b29ceabfcd8416522f4b0dc9807e7e5baf01e
PSJ\aaa95 <psg1478795@naver.com>
2023-01-19 00:38:52 +0900

    초기 세팅(01.19)

---
+++ tiktokclone/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const TikTokApp());
+}
+
+class TikTokApp extends StatelessWidget {
+  const TikTokApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'TikTok Clone',
+      theme: ThemeData(
+        primaryColor: const Color(0xFFE9435A),
+      ),
+      home: Container(
+        color: Colors.white,
+        child: const Text('data'),
+      ),
+    );
+  }
+}
