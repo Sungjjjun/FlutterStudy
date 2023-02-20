3324e10f7ba5fdf058fa24e664a404e3a5910d2e
Sungjjjun <psg1478795@naver.com>
2023-02-20 19:31:32 +0900

    #19.10(02.20)

---
+++ tiktokclone/lib/features/videos/video_recording_screen.dart
@@ -0,0 +1,309 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:tiktokclone/constants/gaps.dart';
+import 'package:tiktokclone/constants/sizes.dart';
+import 'package:tiktokclone/features/videos/video_preview_screen.dart';
+
+class VideoRecordingScreen extends StatefulWidget {
+  const VideoRecordingScreen({super.key});
+
+  @override
+  State<VideoRecordingScreen> createState() => _VideoRecordingScreenState();
+}
+
+class _VideoRecordingScreenState extends State<VideoRecordingScreen>
+    with TickerProviderStateMixin, WidgetsBindingObserver {
+  bool _hasPermission = false;
+  bool _isSelfieMode = false;
+  late FlashMode _flashMode;
+
+  late CameraController _cameraController;
+  late final AnimationController _buttonAnimationController =
+      AnimationController(
+    vsync: this,
+    duration: const Duration(
+      milliseconds: 300,
+    ),
+  );
+  late final AnimationController _progressAnimationController =
+      AnimationController(
+    vsync: this,
+    duration: const Duration(
+      seconds: 10,
+    ),
+    upperBound: 1.0,
+    lowerBound: 0.0,
+  );
+
+  late final Animation<double> _buttonAnimation =
+      Tween(begin: 1.0, end: 1.3).animate(_buttonAnimationController);
+
+  Future<void> initCamera() async {
+    final cameras = await availableCameras();
+    if (cameras.isEmpty) return;
+
+    _cameraController = CameraController(
+      cameras[_isSelfieMode ? 1 : 0],
+      ResolutionPreset.ultraHigh,
+      enableAudio: false,
+    );
+
+    await _cameraController.initialize();
+    await _cameraController.prepareForVideoRecording(); // only for iOS
+
+    _flashMode = _cameraController.value.flashMode;
+  }
+
+  Future<void> initPermission() async {
+    final cameraPermission = await Permission.camera.request();
+    final micPermission = await Permission.microphone.request();
+
+    final cameraDenied =
+        cameraPermission.isDenied || cameraPermission.isPermanentlyDenied;
+    final micDenied =
+        micPermission.isDenied || micPermission.isPermanentlyDenied;
+
+    if (!cameraDenied && !micDenied) {
+      await initCamera();
+      setState(() {
+        _hasPermission = true;
+      });
+    }
+  }
+
+  Future<void> _setFlashMode(FlashMode newFlashMode) async {
+    await _cameraController.setFlashMode(newFlashMode);
+    setState(() {
+      _flashMode = newFlashMode;
+    });
+  }
+
+  Future<void> toggleSelfieMode() async {
+    _isSelfieMode = !_isSelfieMode;
+    await initCamera();
+    setState(() {});
+  }
+
+  Future<void> _startRecording(TapDownDetails _) async {
+    if (_cameraController.value.isRecordingVideo) return;
+
+    await _cameraController.startVideoRecording();
+    _buttonAnimationController.forward();
+    _progressAnimationController.forward();
+  }
+
+  Future<void> _stopRecording() async {
+    if (!_cameraController.value.isRecordingVideo) return;
+
+    _buttonAnimationController.reverse();
+    _progressAnimationController.reset();
+
+    final video = await _cameraController.stopVideoRecording();
+
+    if (!mounted) return;
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => VideoPreviewScreen(
+          video: video,
+          isPicked: false,
+        ),
+      ),
+    );
+  }
+
+  Future<void> _onPickVideoPressed() async {
+    final video = await ImagePicker().pickVideo(
+      source: ImageSource.gallery,
+    );
+    if (video == null) return;
+
+    if (!mounted) return;
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => VideoPreviewScreen(
+          video: video,
+          isPicked: true,
+        ),
+      ),
+    );
+  }
+
+  @override
+  Future<void> didChangeAppLifecycleState(AppLifecycleState state) async {
+    if (!_hasPermission) return;
+    if (!_cameraController.value.isInitialized) return;
+    if (state == AppLifecycleState.inactive) {
+      _cameraController.dispose();
+    } else if (state == AppLifecycleState.resumed) {
+      await initCamera();
+    }
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+    initPermission();
+    _progressAnimationController.addListener(() {
+      setState(() {});
+    });
+    _progressAnimationController.addStatusListener((status) {
+      if (status == AnimationStatus.completed) _stopRecording();
+    });
+  }
+
+  @override
+  void dispose() {
+    _progressAnimationController.dispose();
+    _buttonAnimationController.dispose();
+    _cameraController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        child: !_hasPermission || !_cameraController.value.isInitialized
+            ? Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    "Initializing....",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: Sizes.size20,
+                    ),
+                  ),
+                  Gaps.v20,
+                  CircularProgressIndicator(
+                    color: Colors.red.shade400,
+                  )
+                ],
+              )
+            : Stack(
+                alignment: Alignment.center,
+                children: [
+                  CameraPreview(_cameraController),
+                  Positioned(
+                    right: Sizes.size11,
+                    top: Sizes.size20,
+                    child: Column(
+                      children: [
+                        IconButton(
+                          onPressed: toggleSelfieMode,
+                          icon: const Icon(
+                            Icons.flip_camera_ios_outlined,
+                            size: Sizes.size36,
+                          ),
+                          color: Colors.white,
+                        ),
+                        Gaps.v10,
+                        IconButton(
+                          onPressed: () => _setFlashMode(FlashMode.off),
+                          icon: const Icon(
+                            Icons.flash_off_rounded,
+                            size: Sizes.size36,
+                          ),
+                          color: _flashMode == FlashMode.off
+                              ? Colors.amber
+                              : Colors.white,
+                        ),
+                        Gaps.v10,
+                        IconButton(
+                          onPressed: () => _setFlashMode(FlashMode.always),
+                          icon: const Icon(
+                            Icons.flash_on_rounded,
+                            size: Sizes.size36,
+                          ),
+                          color: _flashMode == FlashMode.always
+                              ? Colors.amber
+                              : Colors.white,
+                        ),
+                        Gaps.v10,
+                        IconButton(
+                          onPressed: () => _setFlashMode(FlashMode.auto),
+                          icon: const Icon(
+                            Icons.flash_auto_rounded,
+                            size: Sizes.size36,
+                          ),
+                          color: _flashMode == FlashMode.auto
+                              ? Colors.amber
+                              : Colors.white,
+                        ),
+                        Gaps.v10,
+                        IconButton(
+                          onPressed: () => _setFlashMode(FlashMode.torch),
+                          icon: const Icon(
+                            Icons.flashlight_on_rounded,
+                            size: Sizes.size36,
+                          ),
+                          color: _flashMode == FlashMode.torch
+                              ? Colors.amber
+                              : Colors.white,
+                        ),
+                      ],
+                    ),
+                  ),
+                  Positioned(
+                    bottom: Sizes.size40,
+                    width: MediaQuery.of(context).size.width,
+                    child: Row(
+                      children: [
+                        const Spacer(),
+                        GestureDetector(
+                          onTapDown: _startRecording,
+                          onTapUp: (details) => _stopRecording(),
+                          child: ScaleTransition(
+                            scale: _buttonAnimation,
+                            child: Stack(
+                              alignment: Alignment.center,
+                              children: [
+                                SizedBox(
+                                  width: Sizes.size80 + Sizes.size14,
+                                  height: Sizes.size80 + Sizes.size14,
+                                  child: CircularProgressIndicator.adaptive(
+                                    strokeWidth: Sizes.size6,
+                                    value: _progressAnimationController.value,
+                                  ),
+                                ),
+                                Container(
+                                  width: Sizes.size80,
+                                  height: Sizes.size80,
+                                  decoration: BoxDecoration(
+                                    shape: BoxShape.circle,
+                                    color: Colors.red.shade400,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        Expanded(
+                          child: Container(
+                            alignment: Alignment.center,
+                            child: IconButton(
+                              onPressed: _onPickVideoPressed,
+                              icon: const FaIcon(
+                                FontAwesomeIcons.image,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+      ),
+    );
+  }
+}
--- tiktokclone/lib/router.dart
+++ tiktokclone/lib/router.dart
@@ -1,57 +1,11 @@
 import 'package:go_router/go_router.dart';
-import 'package:tiktokclone/features/authentication/email_screen.dart';
-import 'package:tiktokclone/features/authentication/sign_up_screen.dart';
-import 'package:tiktokclone/features/authentication/username_screen.dart';
+import 'package:tiktokclone/features/videos/video_recording_screen.dart';
 
 final router = GoRouter(
   routes: [
     GoRoute(
-      path: SignUpScreen.routeURL,
-      name: SignUpScreen.routeName,
-      builder: (context, state) => const SignUpScreen(),
-      routes: [
-        GoRoute(
-          path: UsernameScreen.routeURL,
-          name: UsernameScreen.routeName,
-          builder: (context, state) => const UsernameScreen(),
-          routes: [
-            GoRoute(
-              path: EmailScreen.routeURL,
-              name: EmailScreen.routeName,
-              builder: (context, state) {
-                final arg = state.extra as EmailScreenArgs;
-                return EmailScreen(username: arg.username);
-              },
-            ),
-          ],
-          // pageBuilder: (context, state) {
-          //   return CustomTransitionPage(
-          //     child: const UsernameScreen(),
-          //     transitionsBuilder: (context, animation, secondaryAnimation, child) {
-          //       return FadeTransition(
-          //         opacity: animation,
-          //         child: ScaleTransition(
-          //           scale: animation,
-          //           child: child,
-          //         ),
-          //       );
-          //     },
-          //   );
-          // },
-        ),
-      ],
+      path: "/",
+      builder: (context, state) => const VideoRecordingScreen(),
     ),
-    // GoRoute(
-    //   path: LoginScreen.routeName,
-    //   builder: (context, state) => const LoginScreen(),
-    // ),
-    // GoRoute(
-    //   path: "/users/:username",
-    //   builder: (context, state) {
-    //     final username = state.params["username"];
-    //     final tab = state.queryParams["show"];
-    //     return UserProfileScreen(username: username!, tab: tab!);
-    //   },
-    // ),
   ],
 );
